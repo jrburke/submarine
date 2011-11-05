163115dc1e2eeaa0e59f6deea1264b78cba3fd31
jrburke <jrburke@gmail.com>
2011-11-04 17:26:34 -0700

    Switching to express to hopefully get better static file serving.

--- server.js
+++ server.js
@@ -16,17 +16,13 @@ requirejs({
 
 requirejs(['require'], function (require) {
 
-  var nodeStatic = require('node-static'),
-      http = require('http'),
-      https = require('https'),
+  var express = require('express'),
       fs = require('fs'),
-      url = require('url'),
       path = require('path'),
       io = require('socket.io'),
-      querystring = require('querystring'),
 
       wwwRoot = './phonegap/www',
-      staticServer = new nodeStatic.Server(wwwRoot),
+      builtWwwRoot = wwwRoot + '/../www-built',
       clients = require('./server/clients'),
       serverUrl = require('./server/serverUrl'),
 
@@ -35,107 +31,51 @@ requirejs(['require'], function (require) {
       isHttps = protocol === 'https',
       port = penv.SUBMARINEPORT || penv.PORT,
 
-      server, listener, socketIoOptions;
+      app, listener, socketIoOptions;
 
   socketIoOptions = {
     transports: ['websocket', 'xhr-polling', 'jsonp-polling', 'htmlfile']
   };
 
-  function listenToRequest(request, response) {
-    //Assuming data is a utf8 string. This will be a problem later,
-    //with image uploads.
-    var data = '';
-
-    request.on('data', function (streamData) {
-      data += streamData;
-    });
-
-    request.on('end', function () {
-      var location = url.parse(request.url, true),
-          contentType = request.headers['Content-Type'] || '',
-          contents, optimizedIndex;
-
-      if (location.pathname.indexOf('/api/') === 0) {
-
-        if (data) {
-          if (contentType.indexOf('application/json') === 0) {
-            data = JSON.parse(data);
-          } else {
-            // Assume application/x-www-form-urlencoded
-            data = querystring.parse(data);
-          }
-        } else {
-          // Data is just the location.query for gets and related methods.
-          data = location.query;
-        }
-
-        // Do the API thing.
-        require(['./server/actions' + location.pathname], function (action) {
-          action(data, function (responseData) {
-            // All responses should be JSON friendly data.
-            var contents = JSON.stringify(responseData, null, '  ');
-            response.writeHead(200, {
-              'Content-Type': 'text/plain',
-              'Content-Length': contents.length
-            });
-            response.write(contents, 'utf8');
-            response.end();
-          }, require, response);
-        });
-
-      } else if (location.pathname === '/' ||
-                 location.pathname === '/index.html') {
-        // Read in index.html and replace remoteServerUrl with real URL
-        contents = fs.readFileSync(wwwRoot + '/index.html', 'utf8');
-        contents = contents.replace(/var remoteServerUrl = '[^']*'/,
-                                    "var remoteServerUrl = '" + serverUrl + "'");
-        response.writeHead(200, {
-          'Content-Type': 'text/html;charset=UTF-8',
-          'Content-Length': contents.length
-        });
-        response.write(contents, 'utf8');
-        response.end();
-
-      } else if (location.pathname === '/js/index.js') {
-        // Serve the optimized contents if available.
-        optimizedIndex = wwwRoot + '/../www-built/js/index.js';
-        if (path.existsSync(optimizedIndex)) {
-          contents = fs.readFileSync(optimizedIndex, 'utf8');
-          response.writeHead(200, {
-            'Content-Type': 'text/javascript;charset=UTF-8',
-            'Content-Length': contents.length
-          });
-          response.write(contents, 'utf8');
-          response.end();
-        } else {
-          staticServer.serve(request, response);
-        }
-      } else {
-        staticServer.serve(request, response);
-      }
-    });
+  // Use built www if it is available.
+  if (path.existsSync(builtWwwRoot)) {
+    wwwRoot = builtWwwRoot;
   }
 
   if (isHttps) {
     socketIoOptions.secure = true;
 
-    server = https.createServer({
+    app = express.createServer({
       key: fs.readFileSync(penv.SUBMARINEKEY),
       cert: fs.readFileSync(penv.SUBMARINECERT),
       requestCert: true,
       ca: [
         fs.readFileSync(penv.SUBMARINECA)
       ]
-    }, listenToRequest);
+    });
   } else {
-    server = http.createServer(listenToRequest);
+    app = express.createServer();
   }
 
-  server.listen(port);
+  // Set up static file serving.
+  app.configure(function () {
+    app.use(express['static'](wwwRoot));
+  });
+
+  //
+  app.get('/', function (req, res) {
+    // Read in index.html and replace remoteServerUrl with real URL
+    var contents = fs.readFileSync(wwwRoot + '/index.html', 'utf8');
+    contents = contents.replace(/var remoteServerUrl = '[^']*'/,
+                                "var remoteServerUrl = '" + serverUrl + "'");
+    res.send(contents);
+  });
+
+  app.listen(port);
 
   console.log('Listening on ' + protocol + '://127.0.0.1:' + port);
 
-  listener = io.listen(server, socketIoOptions);
+  listener = io.listen(app, socketIoOptions);
 
   listener.sockets.on('connection', function (client) {
     //client.send({ buffer: buffer });
